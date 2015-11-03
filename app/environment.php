47860754a3ff9b3acbc0d9324b964ddbf1909c5b
sebastianseidelmann <sebastian.seidelmann@wfp2.com>
2015-11-03 15:56:33 +0100

    [COMMENT] ADDED the phpdoc

--- app/environment.php
+++ app/environment.php
@@ -1,14 +1,41 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: sebastianseidelmann
- * Date: 03.11.15
- * Time: 15:51
+ * Environment configuration file for Base typo3 installation.
+ * @author Sebastian Seidelmann <sebastian.seidelmann@wfp2.com>, wfp:2 GmbH & Co. KG
  */
 
+/**
+ * Defines the application path
+ * @var string
+ */
 define('WFP2_PATH_APP',       realpath(__DIR__) . DIRECTORY_SEPARATOR);
+
+/**
+ * Defines the web path
+ * @var string
+ */
 define('WFP2_PATH_WEB',       realpath(__DIR__ . '/../web/') . DIRECTORY_SEPARATOR);
+
+/**
+ * Defines the src path
+ * @var string
+ */
 define('WFP2_PATH_SRC',       realpath(__DIR__ . '/../src/') . DIRECTORY_SEPARATOR);
+
+/**
+ * Defines the typo3conf path
+ * @var string
+ */
 define('WFP2_PATH_TYPO3CONF', WFP2_PATH_SRC . 'typo3conf' . DIRECTORY_SEPARATOR);
+
+/**
+ * Defines the typo3conf/ext path
+ * @var string
+ */
 define('WFP2_PATH_EXT',       WFP2_PATH_TYPO3CONF . 'ext' . DIRECTORY_SEPARATOR);
+
+/**
+ * Defines the typo3temp path
+ * @var string
+ */
 define('WFP2_PATH_TYPO3TEMP', WFP2_PATH_APP . 'typo3temp' . DIRECTORY_SEPARATOR);
